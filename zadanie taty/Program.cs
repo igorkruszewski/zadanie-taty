a3677e62cbca7bfae1a3738f67aeb7b7b08a0d16
Igor <igorkris719@gmail.com>
2024-03-02 23:26:08 +0100

    jest ok

--- zadanie taty/Program.cs
+++ zadanie taty/Program.cs
@@ -1,1 +1,20 @@
-﻿int IlośćKupców
+﻿int portfel = 0;
+const int WartośćTranzakcji = 100;
+const int WartośćStraty = 1000;
+int WspółczynnikStraty = 100;
+const int cel = 100000;
+
+for (int IlośćKupców = 1; portfel < cel; IlośćKupców++)
+{
+  
+    if (IlośćKupców % WspółczynnikStraty==0)
+    {
+        portfel = portfel - 1000;
+    }
+    else
+    {
+        portfel = portfel + WartośćTranzakcji;
+
+    }
+    Console.WriteLine("Wartosc portfela: " + portfel + ", kupiec numer: " + IlośćKupców);
+}
